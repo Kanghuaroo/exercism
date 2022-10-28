c60a8dea80fc70d131f16ee9119788b5b4d51be0
Kanghuaroo <5035dcmiller@gmail.com>
2022-10-28 08:57:20 -0400

    finished armstrong nums

--- c/armstrong-numbers/armstrong_numbers.c
+++ c/armstrong-numbers/armstrong_numbers.c
@@ -1,1 +1,21 @@
 #include "armstrong_numbers.h"
+#include <math.h>
+
+bool is_armstrong_number(int candidate){
+	//get num digits
+	int numDigits = 0;
+	int temp = candidate;
+	for(numDigits = 0; temp != 0; numDigits++){ temp = temp/10; }
+
+	//sum products
+	int sum = 0;
+	temp = candidate;
+	for(int i = 0; i < numDigits; i++){
+		int digit = temp % 10;
+		int product = (int)pow(digit, numDigits);
+		
+		sum += product;
+		temp = temp/10;
+	}
+	return candidate == sum;
+}
