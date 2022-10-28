04034a48ef1d913238a2b492ae4a9132665c50ad
Kanghuaroo <5035dcmiller@gmail.com>
2022-10-28 11:13:19 -0400

    moved to next proj

--- c/isogram/isogram.c
+++ c/isogram/isogram.c
@@ -1,1 +1,19 @@
 #include "isogram.h"
+#include <stddef.h>
+#include <ctype.h>
+
+
+bool is_isogram(const char phrase[]){
+	if(phrase == NULL){return false;}
+
+	for(int i = 0; phrase[i] != '\0'; i++){
+		if(!isalpha(phrase[i])){ continue; }
+
+		for(int j = i+1; phrase[j] != '\0'; j++){
+			if(tolower(phrase[i]) == tolower(phrase[j])){
+				return false;
+			}
+		}
+	}
+	return true;
+}
